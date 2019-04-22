4aa8c6ea631a0c36b5b1c490c8caf0805839f596
Waleed <waleed_arshad006@hotmail.com>
2019-04-22 21:45:16 +0500

    Refactor main file. Add model for main file.

--- lib/main/main.dart
+++ lib/main/main.dart
@@ -1,16 +1,13 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_pk/caches/user.dart';
 import 'package:flutter_pk/contribution/contribution_dialog.dart';
 import 'package:flutter_pk/global.dart';
 import 'package:flutter_pk/home_master.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_pk/main/main_model.dart';
 import 'package:flutter_pk/theme.dart';
 import 'package:flutter_pk/widgets/full_screen_loader.dart';
 import 'package:flutter_pk/widgets/sprung_box.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sprung/sprung.dart';
@@ -49,14 +46,14 @@ class _MyHomePageState extends State<MyHomePage> {
   bool _showSwipeText = false;
   bool _isFetchingSharedPreferences = false;
 
+  GoogleSignInApi api = GoogleSignInApi();
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
 
-    Firestore.instance.settings(
-      timestampsInSnapshotsEnabled: true,
-    );
+    api.setTimestampsSnapshots();
 
     _getSharedPreferences();
   }
@@ -181,38 +178,17 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Future _handleSignIn() async {
+
     final SharedPreferences prefs = await sharedPreferences;
     userCache.clear();
     prefs.clear();
+
     setState(() => _isLoading = true);
+
     try {
-      GoogleSignInAccount googleUser = await googleSignIn.signIn();
-      GoogleSignInAuthentication googleAuth = await googleUser.authentication;
-      FirebaseUser user = await auth.signInWithGoogle(
-        accessToken: googleAuth.accessToken,
-        idToken: googleAuth.idToken,
-      );
-
-      CollectionReference reference =
-          Firestore.instance.collection(FireStoreKeys.userCollection);
-
-      await reference.document(user.uid).get().then((snap) async {
-        if (!snap.exists) {
-          User _user = User(
-              name: user.displayName,
-              mobileNumber: user.phoneNumber,
-              id: user.uid,
-              photoUrl: user.photoUrl,
-              email: user.email);
-
-          await reference
-              .document(user.uid)
-              .setData(_user.toJson(), merge: true);
-        }
-      });
-      prefs.setString(SharedPreferencesKeys.firebaseUserId, user.uid);
-
-      await userCache.getCurrentUser(user.uid);
+      String userId = await api.handleGoogleSignIn();
+      prefs.setString(SharedPreferencesKeys.firebaseUserId, userId);
+      await userCache.getCurrentUser(userId);
 
       if (!userCache.user.isContributor) {
         await Navigator.of(context).push(
@@ -227,6 +203,7 @@ class _MyHomePageState extends State<MyHomePage> {
         Routes.home_master,
         ModalRoute.withName(Routes.main),
       );
+
     } catch (ex) {
       print(ex);
       Alert(
